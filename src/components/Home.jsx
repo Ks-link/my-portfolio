ef0d8ad26cb89cb9eb7eb173e81f3c0a38d8052b
Kaleb Link <kaleb.s.link@gmail.com>
2024-09-24 15:10:28 -0700

    getting api calls working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Home() {
+  const restPath = restBase + 'pages/9';
+  const [restData, setData] = useState([])
+  const [isLoaded, setLoadStatus] = useState(false)
+
   return (
     <h1>Kaleb Link</h1>
   )
