8cd88acaa970b9fd1336f82c0f1f2fce5b519f87
Kaleb Link <kaleb.s.link@gmail.com>
2024-10-11 16:04:01 -0700

    setup finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 function Home() {
-  const restPath = restBase + 'pages/9';
-  const [restData, setData] = useState([])
-  const [isLoaded, setLoadStatus] = useState(false)
+  // const restPath = restBase + 'pages/9';
+  // const [restData, setData] = useState([])
+  // const [isLoaded, setLoadStatus] = useState(false)
 
   return (
-    <h1>Kaleb Link</h1>
+    <>
+      <h1>Kaleb Link</h1>
+      <h2>Front End Web Developer</h2>
+    </>
   )
 }
 
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from '../components/Home'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 function AppRouter() {
     return (
         <BrowserRouter>
-            <Header />
             <main id="main">
                 <Routes>
                     <Route path='/' element={<Home />} />
@@ -15,7 +12,6 @@ function AppRouter() {
                     {/* <Route path='/about' element={<About />} /> */}
                 </Routes>
             </main>
-            <Footer />
         </BrowserRouter>
     )
 }
