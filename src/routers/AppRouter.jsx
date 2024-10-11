704555689ec8ce369d8c7ec0c08978c3240107d8
Kaleb Link <kaleb.s.link@gmail.com>
2024-10-11 16:17:25 -0700

    fixed components

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from '../components/Home'
+import Works from '../components/Works'
+import About from '../components/About'
 
 function AppRouter() {
     return (
