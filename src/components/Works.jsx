1aabf08c70c2060e70c0f0d807714ab8770200e8
Kaleb Link <kaleb.s.link@gmail.com>
2024-10-12 21:21:57 -0700

    added load status, adding api calls

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,50 @@
 import React from 'react'
 import Nav from './Nav'
+import Loading from '../utilities/Loading';
+import { restBase } from '../utilities/Utilities';
+import { useState, useEffect } from 'react';
 
 function Works() {
+    const restPath = restBase + 'pages/11?_embed=true';
+    const [restData, setData] = useState([])
+    const [isLoaded, setLoadStatus] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(restPath)
+            if (response.ok) {
+                const data = await response.json()
+                setData(data)
+                setLoadStatus(true)
+            } else {
+                setLoadStatus(false)
+            }
+        }
+        fetchData()
+    }, [restPath])
+
   return (
-    <article>
-      <h1>Works</h1>
-      <Nav />
-    </article>
+    <>
+      {isLoaded ?
+        <article id={`post-${restData.id}`}>
+          <h1>{restData.title.rendered}</h1>
+          <Nav />
+          <div className="entry-content">
+            <section>
+                <h2>{restData.acf.left_section_heading}</h2>
+                <p>{restData.acf.left_section_content}</p>
+            </section>
+
+            <section>
+                <h2>{restData.acf.right_section_heading}</h2>
+                <p>{restData.acf.right_section_content}</p>
+            </section>
+          </div>
+        </article>
+        :
+        <Loading />
+      }
+    </>
   )
 }
 
---
+++ src/utilities/Loading.jsx
@@ -0,0 +1,11 @@
+import loading from '../assets/loading-01.webp';
+
+const Loading = () => {
+    
+    return (
+        <img src={loading} alt="Loading" className="loading" id="loading" />
+    )
+
+}
+
+export default Loading
