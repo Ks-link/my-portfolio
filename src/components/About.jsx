f436687bae90b24dccf3773eda21c57cf2c05b02
Kaleb Link <kaleb.s.link@gmail.com>
2024-10-25 23:39:12 -0700

    added email link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,6 +51,8 @@ function About() {
               <h1>{restData.title.rendered}</h1>
               <nav className='nav-about'><Nav currentPage="about" /></nav>
               <div className="entry-content" dangerouslySetInnerHTML={{ __html: restData.content.rendered }}></div>
+              <p className='email-text'>Get in touch!</p>
+              <button className='email-link' onClick={() => {navigator.clipboard.writeText("kaleb.s.link@gmail.com")}}><svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m6 18h-3c-.48 0-1-.379-1-1v-14c0-.481.38-1 1-1h14c.621 0 1 .522 1 1v3h3c.621 0 1 .522 1 1v14c0 .621-.522 1-1 1h-14c-.48 0-1-.379-1-1zm1.5-10.5v13h13v-13zm9-1.5v-2.5h-13v13h2.5v-9.5c0-.481.38-1 1-1z" fill-rule="nonzero"/></svg> Copy Email</button>
               <FeaturedImage className='about-image' featuredImageObject={restData._embedded['wp:featuredmedia'][0]} />
           </div>
         </section>
